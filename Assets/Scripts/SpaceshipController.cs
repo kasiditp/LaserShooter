096be6b25fb1317ffc019e14fcfb6bcca292d6c8
Kasidit Phoncharoen <kasidit.p@ku.th>
2017-02-21 09:40:02 +0700

    commit first

---
+++ Assets/Scripts/SpaceshipController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipController : MonoBehaviour {
+
+	public GameObject myBullet;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetAxis("Horizontal") != 0 ) {
+			this.gameObject.transform.Translate (Input.GetAxis ("Horizontal") * Vector3.right * Time.deltaTime *5f);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			Instantiate (myBullet,this.gameObject.transform.position,Quaternion.identity);
+		}
+		
+	}
+}
